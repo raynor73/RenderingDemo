7a01a2aa96e4ad04132f473b6af45a4a6120fc51
Igor Lapin <igor.lapin73@gmail.com>
2020-01-14 17:39:59 +0300

    Add Android Rendering Engine dependency, patch #1

--- app/src/main/java/ilapin/renderingdemo/MainActivity.kt
+++ app/src/main/java/ilapin/renderingdemo/MainActivity.kt
@@ -1,19 +1,12 @@
 package ilapin.renderingdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import ilapin.common.Optional
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val a = Optional.empty<String>()
-        if (a.isEmpty()) {
-            Log.d("!@#", "Empty")
-        }
     }
 }
